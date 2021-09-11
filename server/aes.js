39e28399337c91af58b98805152471a42af04f6f
Diana <dele18607@uvg.edu.gt>
2021-09-10 22:17:57 -0600

    fix error

--- server/aes.js
+++ server/aes.js
@@ -2,12 +2,18 @@ var aes256 = require("aes256");
 
 var secret_key = "uI2ooxtwHeI6q69PS98fx9SWVGbpQohO";
 
-export const to_Encrypt = (text) => {
-  var encrypted = aes256.encrypt(secret_key, text);
-  return encrypted;
+function to_Encrypt(text) {
+    var encrypted = aes256.encrypt(secret_key, text);
+    return encrypted;
 };
 
-export const to_Decrypt = (cipher) => {
-  var decrypted = aes256.decrypt(secret_key, cipher);
-  return decrypted;
+function to_Decrypt(cipher) {
+    var decrypted = aes256.decrypt(secret_key, cipher);
+    return decrypted;
 };
+
+module.exports = {
+    to_Decrypt,
+    to_Encrypt
+};
+
